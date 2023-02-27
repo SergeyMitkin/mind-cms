867fb7fd6b3327ce24b8039f8e579004a61918c5
SergeyMitkin <sergeymitkin@gmail.com>
2023-02-27 10:10:29 +0300

    Исправлен html

--- system/modules/menu/widgets/templates/second-template.php
+++ system/modules/menu/widgets/templates/second-template.php
@@ -10,19 +10,19 @@ if (!empty($menu)) : ?>
                 && array_search($item['id'], $parents) === false
                 && $item['is_subheader']){
                     if($sub_index !== 0) {?>
-                        </div>
+                        </ul>
                     <?}?>
                     <? if ($sub_index == 0) {?>
                     <div class="sub-wrap">
-                        <div class="nav sub-item">
+                        <ul class="nav sub-item">
                             <li class="menu-subheader"><a href="javascript:void(0)"><?= $item['name'] ?></a></li>
                             <?} else if ($sub_index > 0){?>
-                            <div class="nav sub-item">
+                            <ul class="nav sub-item">
                                 <li class="menu-subheader"><a href="javascript:void(0)"><?= $item['name'] ?></a></li>
                                 <?}?>
                                 <? $sub_index++;
                                 if ($sub_index > 0 && $sub_index == count($menu)){?>
-                            </div>
+                            </ul>
                         </div>
                     <?}
                     }
@@ -36,9 +36,9 @@ if (!empty($menu)) : ?>
                                 <div class="link-name"><?= $item['name'] ?></div>
                                 <div class="triangle"></div>
                             </a>
-                            <ul class="nav collapse submenu">
+                            <div class="nav collapse submenu">
                                 <? showSecondTemplate($menu, $item['id'], $parents); ?>
-                            </ul>
+                            </div>
                         </li>
                     <?}
                     }
@@ -57,34 +57,34 @@ if (!empty($menu)) : ?>
                     <div class="link-name">Menu Item B</div>
                     <div class="triangle"></div>
                 </a>
-                <ul class="nav collapse submenu">
+                <div class="nav collapse submenu">
                     <div class="sub-wrap">
-                        <div class="nav sub-item">
+                        <ul class="nav sub-item">
                             <li class="menu-subheader"><a href="javascript:void(0)">SUB-HEADER 1</a></li>
                             <li><a href="javascript:void(0)">Menu Link 1.1</a></li>
                             <li><a href="javascript:void(0)">Menu Link 1.2</a></li>
                             <li><a href="javascript:void(0)">Menu Link 1.3</a></li>
                             <li><a href="javascript:void(0)">Menu Link 1.4</a></li>
                             <li><a href="javascript:void(0)">Menu Link 1.5</a></li>
-                        </div>
-                        <div class="nav sub-item">
+                        </ul>
+                        <ul class="nav sub-item">
                             <li class="menu-subheader"><a href="javascript:void(0)">SUB-HEADER 2</a></li>
                             <li><a href="javascript:void(0)">Menu Link 2.1</a></li>
                             <li><a href="javascript:void(0)">Menu Link 2.2</a></li>
                             <li><a href="javascript:void(0)">Menu Link 2.3</a></li>
                             <li><a href="javascript:void(0)">Menu Link 2.4</a></li>
                             <li><a href="javascript:void(0)">Menu Link 2.5</a></li>
-                        </div>
-                        <div class="nav sub-item">
+                        </ul>
+                        <ul class="nav sub-item">
                             <li class="menu-subheader"><a href="javascript:void(0)">SUB-HEADER 3</a></li>
                             <li><a href="javascript:void(0)">Menu Link 3.1</a></li>
                             <li><a href="javascript:void(0)">Menu Link 3.2</a></li>
                             <li><a href="javascript:void(0)">Menu Link 3.3</a></li>
                             <li><a href="javascript:void(0)">Menu Link 3.4</a></li>
                             <li><a href="javascript:void(0)">Menu Link 3.5</a></li>
-                        </div>
+                        </ul>
                     </div>
-                </ul>
+                </div>
             </li>
             <li><a href="javascript:void(0)">Menu Item C</a></li>
             <li><a href="javascript:void(0)">Menu Item D</a></li>
