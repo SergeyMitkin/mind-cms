e8e83598d9585c00c52454a606433cb18b3196cd
SergeyMitkin <sergeymitkin@gmail.com>
2023-03-01 16:48:19 +0300

    showDropdownMenu

--- www/assets/modules/menu/js/first-template.js
+++ www/assets/modules/menu/js/first-template.js
@@ -1,20 +1,17 @@
+function showDropdownMenu(el){
+    if ( $(el).next(".submenu").is(":visible") ){
+        $(el).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
+        $(el).next().hide();
+    } else {
+        $(el).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-down\" aria-hidden=\"true\"></i>");
+        $(el).next().show();
+    }
+}
+
 $(function (){
    $("li.dropdown a").on("touchstart", function (){
-       if ( $(this).next(".submenu").is(":visible") ){
-           $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
-           $(this).next().hide();
-       } else {
-           $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-down\" aria-hidden=\"true\"></i>");
-           $(this).next().show();
-       }
-   }).on("click", function (e){
-       e.preventDefault();
-       if ( $(this).next(".submenu").is(":visible") ){
-           $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
-           $(this).next().hide();
-       } else {
-           $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-down\" aria-hidden=\"true\"></i>");
-           $(this).next().show();
-       }
+        showDropdownMenu(this);
+   }).on("click", function (){
+       showDropdownMenu(this);
    });
 });
