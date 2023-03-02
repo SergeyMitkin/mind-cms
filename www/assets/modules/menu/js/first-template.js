17030164ddb0ad5da07f68c7376d171c5f980122
SergeyMitkin <sergeymitkin@gmail.com>
2023-03-02 08:39:21 +0300

    resize

--- www/assets/modules/menu/js/first-template.js
+++ www/assets/modules/menu/js/first-template.js
@@ -1,12 +1,37 @@
 $(function (){
-    $("li.dropdown > a").on("touchstart click", function (e){
-        e.preventDefault();
-        if ( $(this).siblings(".submenu").is(":visible") ){
-            $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
-            $(this).siblings(".submenu").hide();
+    if ( $(".first-menu-mobile").is(":visible") ) {
+        $("li.dropdown > a").on("touchstart click", function(e){
+            e.preventDefault();
+            if ( $(this).siblings(".submenu").is(":visible") ){
+                $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
+                $(this).siblings(".submenu").hide();
+            } else {
+                $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-down\" aria-hidden=\"true\"></i>");
+                $(this).siblings(".submenu").show();
+            }
+        });
+    }
+
+    $( window ).resize(function() {
+        if ( !$(".first-menu-mobile").is(":visible")) {
+            $("li.dropdown > a").off("touchstart click").children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
         } else {
-            $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-down\" aria-hidden=\"true\"></i>");
-            $(this).siblings(".submenu").show();
+            $("li.dropdown > a").on("touchstart click", function(e){
+                e.preventDefault();
+                if ( $(this).siblings(".submenu").is(":visible") ){
+                    $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
+                    $(this).siblings(".submenu").hide();
+                } else {
+                    $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-down\" aria-hidden=\"true\"></i>");
+                    $(this).next("ul.submenu").show();
+                }
+            }).each(function (){
+                if ( !$(this).siblings(".submenu").is(":visible") ){
+                    $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-right\" aria-hidden=\"true\"></i>");
+                } else {
+                    $(this).children(".fa-angle-wrap").html("<i class=\"fa fa-angle-double-down\" aria-hidden=\"true\"></i>");
+                }
+            });
         }
     });
 });
