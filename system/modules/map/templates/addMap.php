2886cc56618fe9be9b1108e58a44555bef171873
SergeyMitkin <sergeymitkin@gmail.com>
2023-04-05 16:17:33 +0300

    actionAdd

---
+++ system/modules/map/templates/addMap.php
@@ -0,0 +1,15 @@
+<div class="panel">
+    <div class="panel-heading clearfix">
+        <?= $data['topmenu']; ?>
+    </div>
+
+    <div class="panel-body">
+<!--        <div id="ContentZone"></div>-->
+<!--        <script>-->
+<!--            let APP = "";-->
+<!--            document.addEventListener("DOMContentLoaded", function () {-->
+<!--                APP = EMBMap(document.getElementById('ContentZone'))-->
+<!--            });-->
+<!--        </script>-->
+    </div>
+</div>
