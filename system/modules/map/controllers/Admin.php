d3d67386588e253f7e66a03ac82a5bd7914edf50
SergeyMitkin <sergeymitkin@gmail.com>
2023-04-06 14:18:52 +0300

    uploadBackground при insert

--- system/modules/map/controllers/Admin.php
+++ system/modules/map/controllers/Admin.php
@@ -36,52 +36,12 @@ class Admin extends Controller
     function actionAdd($id = false)
     {
         if (!empty($_POST)) {
-
             if (isset($_FILES['background']) && $_FILES['background']['error'] === 0) {
-                $file_name = mb_substr($_FILES['background']['name'], 0, mb_stripos($_FILES['background']['name'], '.'));
-                $file_type = mb_substr($_FILES['background']['type'], mb_stripos($_FILES['background']['type'], '/')+1);
-                $file_tmp_name = $_FILES['background']['tmp_name'];
-                $img_dir = '/assets/modules/map/img/';
-                $array_ext_access = array('png', 'jpeg');  //Разрешённые расширения
-
-                if (array_search($file_type, $array_ext_access) !== false) {
-                    // Проверка background_path на уникальность в дирректории
-
-                    // Кириллические символы заменяются латинскими
-                    if( preg_match("/[А-Яа-я]/", $file_name) ) {
-                        $file_name = Tools::cyrToLat($file_name);
-                    }
-                    $file_path = $_SERVER['DOCUMENT_ROOT'] . $img_dir . $file_name . '.' . $file_type;
-                    if (!file_exists($file_path)) {
-                        move_uploaded_file($file_tmp_name, $file_path);
-                    } else {
-                        $file_name = $file_name . '-' . date('U');
-                        $file_path = $_SERVER['DOCUMENT_ROOT'] . $img_dir . $file_name. '.' . $file_type;
-                        move_uploaded_file($file_tmp_name, $file_path);
-                    }
-
-                    $data['name'] = $_POST['name'];
-                    $data['background_path'] = $file_name . '.' . $file_type;
-
-                    Map::instance()->save($data);
-                }
+                $data['name'] = $_POST['name'];
+                $data['background_path'] = Map::instance()->uploadBackground();
+                Map::instance()->save($data);
             }
-
             header('Location:/map/admin');
-            // К url добавляется слэш
-//            if (isset($_POST['url']) && substr($_POST['url'], 0, 1)!="/") {
-//                $_POST['url'] = "/".$_POST['url'];
-//            }
-//
-//            Menu::instance()->saveMenu('add', $_POST);
-//
-//            // Направляем на страницу корневой категории
-//            if (isset($_POST['parent_id']) && isset($_POST['menu_id'])){
-//                $root_id = Menu::getParentIdByMenuId($_POST['menu_id']);
-//                header('Location:/menu/admin/listmenu/' . $root_id);
-//            } else {
-//                header('Location:/menu/admin');
-//            }
         }
         else {
             $this->html->setJs('/assets/modules/map/js/addMap.js');
--- system/modules/map/models/Map.php
+++ system/modules/map/models/Map.php
@@ -25,4 +25,35 @@ class Map extends Model
         }
         return $this;
     }
+
+    public function uploadBackground()
+    {
+        $file_name = mb_substr(basename($_FILES['background']['name']), 0, mb_stripos(basename($_FILES['background']['name']), '.'));
+        $file_extension = mb_substr(basename($_FILES['background']['name']), mb_stripos(basename($_FILES['background']['name']), '.')+1);
+        $file_type = mb_substr($_FILES['background']['type'], mb_stripos($_FILES['background']['type'], '/')+1);
+        $file_tmp_name = $_FILES['background']['tmp_name'];
+        $img_dir = '/assets/modules/map/img/';
+        $array_ext_access = array('png', 'jpeg');  //Разрешённые расширения
+
+        if (array_search($file_type, $array_ext_access) !== false) {
+            // Кириллические символы заменяются латинскими
+            if (preg_match("/[А-Яа-я]/", $file_name)) {
+                $file_name = Tools::cyrToLat($file_name);
+            }
+
+            $file_path = $_SERVER['DOCUMENT_ROOT'] . $img_dir . $file_name . '.' . $file_extension;
+            // Если файл с таким именем существует, к имени добавляется timestamp
+            if (!file_exists($file_path)) {
+                move_uploaded_file($file_tmp_name, $file_path);
+            } else {
+                $file_name = $file_name . '-' . date('U');
+                $file_path = $_SERVER['DOCUMENT_ROOT'] . $img_dir . $file_name . '.' . $file_extension;
+                move_uploaded_file($file_tmp_name, $file_path);
+            }
+
+            return $file_name . '.' . $file_extension;
+        } else {
+            return '';
+        }
+    }
 }
