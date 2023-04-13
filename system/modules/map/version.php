018ede967ab696e6bf4c91a915c4179d30536372
SergeyMitkin <sergeymitkin@gmail.com>
2023-04-13 08:51:05 +0300

    version.php

--- system/modules/map/version.php
+++ system/modules/map/version.php
@@ -1,14 +1,14 @@
 <?php
 return [
-    'version' => '1.0',
+    'version' => '1.6',
     'ModuleInfo' => [
-        'name' => 'map',
-        'version_description' => 'Создание, редактирование, просмотр карт',
-        'link_home' => '/map',
+        'name' => 'menu',
+        'version_description' => 'Выбор шаблонов для меню',
+        'link_home' => '/menu',
     ],
     'Folders' => [ // папки для архивации. По умолчанию system/modules/NameModule и www/assets/modules/NameModule
-        'system/modules/map',
-        'www/assets/modules/map',
+        'system/modules/menu',
+        'www/assets/modules/menu',
     ],
     'requireModules' => [ // дополнительные модули, которые требует данный модуль чтобы работать. По умолчанию их нет
     ],
